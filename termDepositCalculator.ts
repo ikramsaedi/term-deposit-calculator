ebf483bbf54eff1c6b30fc4f9eeb1289e321bd8f
Ikram Saedi <ikramsaedi@gmail.com>
2024-09-25 08:39:25 +1000

    feature: support calculating term deposits that are paid at maturity and term deposits that are compounded monthly

--- termDepositCalculator.ts
+++ termDepositCalculator.ts
@@ -1,13 +1,88 @@
 import yargs from "yargs";
+// NOTE currently all investment terms are in years
+// TODO support investment terms in months
 
 const argv = yargs(process.argv.slice(2))
   .options({
+    // demandOption means that this option MUST be specified
+    // if it is not specified, an error will be thrown
     "start-deposit": { type: "number", demandOption: true },
     "interest-rate": { type: "number", demandOption: true },
     "investment-term": { type: "number", demandOption: true },
     "interest-paid": {
+      // Only these values can be used for interest-paid. Any other
+      // values will throw an error
       choices: ["monthly", "quarterly", "annually", "at maturity"],
       demandOption: true,
     },
   })
+  // This prevents unknown arguments from being passed through to the CLI
+  .strict()
+  // Automatically configures -h / --help flag
+  .help()
+  // Needed to parse
   .parseSync();
+
+switch (argv["interest-paid"]) {
+  case "at maturity":
+    const newTermDepositValue = calculateTermDepositAtMaturity(
+      argv["start-deposit"],
+      argv["interest-rate"],
+      argv["investment-term"]
+    );
+
+    console.log("maturity", newTermDepositValue);
+    break;
+  case "monthly":
+    const newTermDepositValue2 = calculateTermDepositMonthly(
+      argv["start-deposit"],
+      argv["interest-rate"],
+      argv["investment-term"]
+    );
+    console.log("monthly", newTermDepositValue2);
+    break;
+  default:
+    console.log("help! not supposed to be here!");
+}
+/**
+ *
+ * @param startDeposit
+ * @param interestRate
+ * @param investmentTerm
+ * Uses simple interest formula
+ */
+function calculateTermDepositAtMaturity(
+  startDeposit: number,
+  interestRate: number,
+  investmentTerm: number
+) {
+  const simpleInterest = (startDeposit * interestRate * investmentTerm) / 100;
+  return startDeposit + simpleInterest;
+}
+
+function calculateTermDepositMonthly(
+  startDeposit: number,
+  interestRate: number,
+  investmentTerm: number
+) {
+  return calculateCompoundInterest(
+    startDeposit,
+    interestRate,
+    investmentTerm,
+    12
+  );
+}
+
+function calculateCompoundInterest(
+  startDeposit: number,
+  interestRate: number,
+  investmentTerm: number,
+  timesCompounded: number
+) {
+  const rateAsPercentage = interestRate / 100;
+  const compoundInterest =
+    (1 + rateAsPercentage / timesCompounded) **
+    (timesCompounded * investmentTerm);
+
+  return startDeposit * compoundInterest;
+}
